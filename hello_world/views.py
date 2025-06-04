7eb470bc974f87b68f5dd840d82395829337aa40
Is2dann <b.daniel.oliver@gmail.com>
2025-06-04 17:52:21 +0100

    Add index request code for GET and POST methods

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, Rio!")
+
+    if request.method == "POST":
+        return HttpResponse("This was a POST request.")
+    else:
+        return HttpResponse("request.method")
